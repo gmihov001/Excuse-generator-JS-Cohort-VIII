a99ec03d7d95119ea7ac17a7a392f3f4de87d10f
George Mihov <gmihov001@gmail.com>
2020-07-13 03:05:47 +0000

    Initial commit

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,9 @@
 
 import "../assets/img/rigo-baby.jpg";
 import "../assets/img/4geeks.ico";
-//import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let string = "Hello Rigo from the console!";
+  document.querySelector("#the-excuse").innerHTML = string;
 };
