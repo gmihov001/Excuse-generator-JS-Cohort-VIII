5b7477ec077a681629908e20b1b7dc88292ba1a8
George Mihov <gmihov001@gmail.com>
2020-12-02 18:54:55 +0000

    updates

--- src/js/index.js
+++ src/js/index.js
@@ -5,24 +5,27 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 // window.onload = function() {};
+const excuseGenerator = () => {
+  let who = ["My parrot ", "My grandma ", "My dog ", "My neighbor "];
+  let did_what = [
+    " ate my shoes ",
+    " crashed my car ",
+    "made a big mess in his cage ",
+    "was chasing squirrels"
+  ];
+  let when = [
+    " yesterday.",
+    " the day before.",
+    "when I was trying to leave.",
+    "when you called."
+  ];
 
-let who = ["My parrot ", "My grandma ", "My dog ", "My neighbor "];
-let did_what = [
-  " ate my shoes ",
-  " crashed my car ",
-  "made a big mess in his cage ",
-  "was chasing squirrels"
-];
-let when = [
-  " yesterday.",
-  " the day before.",
-  "when I was trying to leave.",
-  "when you called."
-];
+  let outcome =
+    who[Math.floor(Math.random() * who.length)] +
+    did_what[Math.floor(Math.random() * did_what.length)] +
+    when[Math.floor(Math.random() * when.length)];
 
-let outcome =
-  who[Math.floor(Math.random() * who.length)] +
-  did_what[Math.floor(Math.random() * did_what.length)] +
-  when[Math.floor(Math.random() * when.length)];
+  return outcome;
+};
 
-document.querySelector("#the-excuse").innerHTML = outcome;
+document.querySelector("#the-excuse").innerHTML = excuseGenerator();
