706a84b6d799be85d909c344c27ed6ab5d574f7d
George Mihov <gmihov001@gmail.com>
2020-12-02 18:51:48 +0000

    updates

--- src/js/index.js
+++ src/js/index.js
@@ -4,15 +4,25 @@ import "../assets/img/rigo-baby.jpg";
 import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
-window.onload = function() {
-  let who = ["My parrot ", "My grandma "];
-  let did_what = [" ate my shoes ", " crashed my car "];
-  let when = [" yesterday.", " the day before."];
+// window.onload = function() {};
 
-  let outcome =
-    who[Math.floor(Math.random() * who.length)] +
-    did_what[Math.floor(Math.random() * did_what.length)] +
-    when[Math.floor(Math.random() * when.length)];
+let who = ["My parrot ", "My grandma ", "My dog ", "My neighbor "];
+let did_what = [
+  " ate my shoes ",
+  " crashed my car ",
+  "made a big mess in his cage ",
+  "was chasing squirrels"
+];
+let when = [
+  " yesterday.",
+  " the day before.",
+  "when I was trying to leave.",
+  "when you called."
+];
 
-  document.querySelector("#the-excuse").innerHTML = outcome;
-};
+let outcome =
+  who[Math.floor(Math.random() * who.length)] +
+  did_what[Math.floor(Math.random() * did_what.length)] +
+  when[Math.floor(Math.random() * when.length)];
+
+document.querySelector("#the-excuse").innerHTML = outcome;
